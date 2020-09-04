5ecafa31d8a33bad931e6788ed131bb5eb1e2310
NadjaGit99 <nadjaeggi@web.de>
2020-09-04 15:20:20 +0200

    Extention: Livingroom

--- src/main.java
+++ src/main.java
@@ -1,5 +1,6 @@
 public class main {
     public static void main (String[] args){
         System.out.println("In my house there are following rooms:");
+	System.out.println("- Livingroom");
     }
 }
