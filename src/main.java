21a13cd15d77bf536d41e32becc4d3005db60620
NadjaGit99 <nadjaeggi@web.de>
2020-09-15 09:24:09 +0200

    improve indention

--- src/main.java
+++ src/main.java
@@ -1,13 +1,13 @@
 public class main {
     public static void main (String[] args){
         System.out.println("In my house there are following rooms:");
-	System.out.println("- livingroom");
-	System.out.println("- bathroom");
-	System.out.println("- kitchen");
-	System.out.println("- bedroom\n");
-	System.out.println("These furniture are the most importent ones:");
-	System.out.println("- bed");
-	System.out.println("- wardrobe");	 
-	System.out.println("- sofa");
-    } 
+		System.out.println("- livingroom");
+		System.out.println("- bathroom");
+		System.out.println("- kitchen");
+		System.out.println("- bedroom\n");
+		System.out.println("These furniture are the most importent ones:");
+		System.out.println("- bed");
+		System.out.println("- wardrobe");	 
+		System.out.println("- sofa");
+    }  
 }
